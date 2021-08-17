b57ffa2f6e9c94fa0fd198045fc7cda796a6d9a3
sanek98cv <73754789+sanek98cv@users.noreply.github.com>
2021-08-17 12:49:17 +0300

    Initial commit

---
+++ src/main/java/com/task/nominatim/NominatimApplication.java
@@ -0,0 +1,13 @@
+package com.task.nominatim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NominatimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NominatimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/nominatim/NominatimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.nominatim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NominatimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
